c55f03d4436dc5ee5106a406997940b15f645942
QuangDuong2903 <quangduongptsc@gmail.com>
2024-09-05 00:04:14 +0700

    update

---
+++ rule-service/src/test/java/com/genesis/rule_service/RuleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genesis.rule_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
